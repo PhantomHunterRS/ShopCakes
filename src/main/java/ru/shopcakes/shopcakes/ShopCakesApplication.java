530b4ed9618a7d3b4d77b99004d9c2f4f363f441
phantomhunter <ASerFilyakov@mail.ru>
2022-12-14 21:02:40 +0300

    Initial commit

---
+++ src/main/java/ru/shopcakes/shopcakes/ShopCakesApplication.java
@@ -0,0 +1,13 @@
+package ru.shopcakes.shopcakes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopCakesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopCakesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/shopcakes/shopcakes/ShopCakesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shopcakes.shopcakes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopCakesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
